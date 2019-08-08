b7d080f6020304002f5f3b36ecc176c6e6a1d44e
kunaldawn <kunal.dawn@gmail.com>
2019-08-09 01:50:27 +0530

    fixes

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd prometheus && docker build -t kunaldawn/hyperion-prometheus .
-cd .. && cd node-exporter && docker build -t kunaldawn/hyperion-node-exporter .
-cd .. && cd docker-exporter && docker build -t kunaldawn/hyperion-docker-exporter .
-cd .. && cd alertmanager && docker build -t kunaldawn/hyperion-alertmanager .
-cd .. && cd grafana && docker build -t kunaldawn/hyperion-grafana .
-cd .. && cd metric-discovery && docker build -t kunaldawn/hyperion-metric-discovery .
+cd prometheus && docker build -t kunaldawn/hyperion-prometheus:v1.0 .
+cd .. && cd node-exporter && docker build -t kunaldawn/hyperion-node-exporter:v1.0 .
+cd .. && cd docker-exporter && docker build -t kunaldawn/hyperion-docker-exporter:v1.0 .
+cd .. && cd alertmanager && docker build -t kunaldawn/hyperion-alertmanager:v1.0 .
+cd .. && cd grafana && docker build -t kunaldawn/hyperion-grafana:v1.0 .
+cd .. && cd metric-discovery && docker build -t kunaldawn/hyperion-metric-discovery:v1.0 .
 cd ..
